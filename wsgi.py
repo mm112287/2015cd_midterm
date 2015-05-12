0a50ef3e7a71757be18628023eda797ea76584ce
chiaming yen <chiamingyen@gmail.com>
2015-05-12 11:40:00 +0800

    w11

--- wsgi.py
+++ wsgi.py
@@ -132,7 +132,32 @@ class Hello(object):
         <input type=\"submit\" value=\"drawspur\">
         <input type=\"reset\" value=\"重填\">
     </form>
-        
+
+    <form method=\"post\" action=\"gear7\">
+        <fieldset>
+        <legend>40223124的7顆齒輪嚙合參數表單(會畫圖):</legend>
+        齒數1:<br />
+        <input type=\"text\" name=\"ng1\"><br />
+        齒數2:<br />
+        <input type=\"text\" name=\"ng2\"><br />
+        齒數3:<br />
+        <input type=\"text\" name=\"ng3\"><br />
+        齒數4:<br />
+        <input type=\"text\" name=\"ng4\"><br />
+        齒數5:<br />
+        <input type=\"text\" name=\"ng5\"><br />
+        齒數6:<br />
+        <input type=\"text\" name=\"ng6\"><br />
+        齒數7:<br />
+        <input type=\"text\" name=\"ng7\"><br />
+        </select>
+        模數:<br />
+        <input type=\"text\" name=\"m\"><br />
+        壓力角(>33時會有錯誤):<br />
+        <input type=\"text\" name=\"inp2\"><br />
+        <input type=\"submit\" value=\"gear7\">
+        <input type=\"reset\" value=\"重填\">
+    </form>
     <hr>
     <!-- 以下在網頁內嵌 Brython 程式 -->
     <script type="text/python">
@@ -371,6 +396,217 @@ class Hello(object):
         outString += "<br />"
         outString += self.menuLink()
         return outString
+    #@+node:2015.20150512113138.1: *3* gear7
+    @cherrypy.expose
+    # N 為齒數, M 為模數, P 為壓力角
+    def gear7(self, m=None, ng1=None, ng2=None, ng3=None, ng4=None, ng5=None, ng6=None, ng7=None, inp2=None):
+        outString = ""
+        outString +="藍色，齒數1:"+ng1
+        outString += "<br />"
+        outString +="黑色，齒數2:"+ng2
+        outString += "<br />"
+        outString +="紅色，齒數3:"+ng3
+        outString += "<br />"
+        outString +="紫色，齒數4:"+ng4
+        outString += "<br />"
+        outString +="黃色，齒數5:"+ng5
+        outString += "<br />"
+        outString +="綠色，齒數6:"+ng6
+        outString += "<br />"
+        outString +="橘色，齒數7:"+ng7
+        outString += "<br />"
+        outString +="模數:"+m
+        outString += "<br />"
+        outString +="壓力角:"+inp2
+        outString += "<br />"
+        outString += self.menuLink()
+        outString += '''
+    <!DOCTYPE html> 
+    <html>
+    <head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8">
+    <!-- 載入 brython.js -->
+    <script type="text/javascript" src="/static/Brython3.1.1-20150328-091302/brython.js"></script>
+    <script src="/static/Cango2D.js" type="text/javascript"></script>
+    <script src="/static/gearUtils-04.js" type="text/javascript"></script>
+    </head>
+    <!-- 啟動 brython() -->
+    <body onload="brython()">
+
+    <!-- 以下為 canvas 畫圖程式 -->
+    <script type="text/python">
+    # 從 browser 導入 document
+    from browser import document
+    from math import *
+    # 請注意, 這裡導入位於 Lib/site-packages 目錄下的 spur.py 檔案
+    import spur
+
+    # 準備在 id="plotarea" 的 canvas 中繪圖
+    canvas = document["plotarea"]
+    ctx = canvas.getContext("2d")
+    # 以下利用 spur.py 程式進行繪圖, 接下來的協同設計運算必須要配合使用者的需求進行設計運算與繪圖
+    # 其中並將工作分配給其他組員建立類似 spur.py 的相關零件繪圖模組
+    # midx, midy 為齒輪圓心座標, rp 為節圓半徑, n 為齒數, pa 為壓力角, color 為線的顏色
+    # Gear(midx, midy, rp, n=20, pa=20, color="black"):
+    # 模數決定齒的尺寸大小, 囓合齒輪組必須有相同的模數與壓力角
+    # 壓力角 pa 單位為角度
+    pa = '''+str(inp2)+'''
+
+    # m 為模數
+    m = '''+str(m)+'''
+
+    # 齒輪齒數
+    n_g1 = '''+str(ng1)+'''
+    n_g2 = '''+str(ng2)+'''
+    n_g3 = '''+str(ng3)+'''
+    n_g4 = '''+str(ng4)+'''
+    n_g5 = '''+str(ng5)+'''
+    n_g6 = '''+str(ng6)+'''
+    n_g7 = '''+str(ng7)+'''
+
+
+    # 計算兩齒輪的節圓半徑
+    rp_g1 = m*n_g1/2
+    rp_g2 = m*n_g2/2
+    rp_g3 = m*n_g3/2
+    rp_g4 = m*n_g4/2
+    rp_g5 = m*n_g5/2
+    rp_g6 = m*n_g6/2
+    rp_g7 = m*n_g7/2
+
+    # 繪圖齒輪的圓心座標,假設排列成水平, 表示各齒輪圓心 y 座標相同
+    x_g1 = 400
+    y_g1 = 400
+
+    x_g2 = x_g1 + rp_g1 + rp_g2
+    y_g2 = y_g1
+
+    x_g3 = x_g2 + rp_g2 + rp_g3
+    y_g3 = y_g1
+
+    x_g4 = x_g3 + rp_g3 + rp_g4
+    y_g4 = y_g1
+
+    x_g5 = x_g4 + rp_g4 + rp_g5 
+    y_g5 = y_g1
+
+    x_g6 = x_g5 + rp_g5 + rp_g6 
+    y_g6 = y_g1
+
+    x_g7 = x_g6 + rp_g6+ rp_g7
+    y_g7 = y_g1
+    #齒輪嚙合的旋轉角
+    # 將第1齒輪順時鐘轉 90 度
+    th1 = pi/2
+
+    # 將第2齒輪逆時鐘轉 90 度之後, 再多轉一齒, 以便與第1齒輪進行囓合
+    th2 = -pi/2-pi/n_g2
+
+    # 將第3齒輪逆時鐘轉 90 度之後, 再往回轉第2齒輪定位帶動轉角, 然後再逆時鐘多轉一齒, 以便與第2齒輪進行囓合
+    # 第1個 -pi/2 為將原先垂直的第3齒輪定位線逆時鐘旋轉 90 度
+    # -pi/n_g3 則是第3齒與第2齒定位線重合後, 必須再逆時鐘多轉一齒的轉角, 以便進行囓合
+    # (pi+pi/n_g2)*n_g2/n_g3 則是第2齒原定位線為順時鐘轉動 90 度, 
+    # pi+pi/n_g2 為第2齒輪從順時鐘轉 90 度之後, 必須配合目前的標記線所作的齒輪 2 轉動角度, 要轉換到齒輪3 的轉動角度
+    # 必須乘上兩齒輪齒數的比例, 若齒輪2 大, 則齒輪3 會轉動較快
+    # 但是第2齒輪為了與第1齒輪囓合, 已經距離定位線, 多轉了 180 度, 再加上第2齒輪的一齒角度, 因為要帶動第3齒輪定位, 
+    # 這個修正角度必須要再配合第2齒與第3齒的轉速比加以轉換成第3齒輪的轉角, 因此乘上 n_g2/n_g3
+    th3 = -pi/2-pi/n_g3+(pi+pi/n_g2)*n_g2/n_g3
+
+    th4 = -pi/2-pi/n_g4+(pi+pi/n_g3)*n_g3/n_g4-(pi+pi/n_g2)*n_g2/n_g4
+    th5 = -pi/2-pi/n_g5+(pi+pi/n_g4)*n_g4/n_g5-(pi+pi/n_g3)*n_g3/n_g5+(pi+pi/n_g2)*n_g2/n_g5
+    th6 = -pi/2-pi/n_g6+(pi+pi/n_g5)*n_g5/n_g6-(pi+pi/n_g4)*n_g4/n_g6+(pi+pi/n_g3)*n_g3/n_g6-(pi+pi/n_g2)*n_g2/n_g6
+    th7 = -pi/2-pi/n_g7+(pi+pi/n_g6)*n_g6/n_g7-(pi+pi/n_g5)*n_g5/n_g7+(pi+pi/n_g4)*n_g4/n_g7-(pi+pi/n_g3)*n_g3/n_g7+(pi+pi/n_g2)*n_g2/n_g7
+
+
+
+    # 使用 ctx.save() 與 ctx.restore() 以確保各齒輪以相對座標進行旋轉繪圖
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(x_g1,y_g1)
+    ctx.rotate(pi/2)
+    # put it back
+    ctx.translate(-x_g1,-y_g1)
+    spur.Spur(ctx).Gear(x_g1,y_g1,rp_g1,n_g1, pa, "blue")
+    ctx.restore()
+    ctx.font = "10px Verdana";
+    ctx.fillText("組員:24號袁丞宗所繪製",x_g1-60, y_g1-10);
+
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(x_g2,y_g2)
+    # rotate to engage
+    ctx.rotate(th2)
+    # put it back
+    ctx.translate(-x_g2,-y_g2)
+    spur.Spur(ctx).Gear(x_g2,y_g2,rp_g2,n_g2, pa, "black")
+    ctx.restore()
+
+    # 假如第3齒也要進行囓合, 又該如何進行繪圖?
+
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(x_g3, y_g3)
+    # rotate to engage
+
+
+    ctx.rotate(th3)
+    # put it back
+    ctx.translate(-x_g3, -y_g3)
+    spur.Spur(ctx).Gear(x_g3, y_g3, rp_g3, n_g3, pa, "red")
+    ctx.restore()
+
+    # 假如第3齒也要進行囓合, 又該如何進行繪圖?
+    # 將第3齒輪逆時鐘轉 90 度之後, 再往回轉第2齒輪定位帶動轉角, 然後再逆時鐘多轉一齒, 以便與第2齒輪進行囓合
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(x_g4, y_g4)
+    # rotate to engage
+    ctx.rotate(th4)
+    # put it back
+    ctx.translate(-x_g4, -y_g4)
+    spur.Spur(ctx).Gear(x_g4, y_g4, rp_g4, n_g4, pa, "purple")
+    ctx.restore()
+
+    #5
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(x_g5, y_g5)
+    # rotate to engage
+    ctx.rotate(th5)
+    # put it back
+    ctx.translate(-x_g5, -y_g5)
+    spur.Spur(ctx).Gear(x_g5, y_g5, rp_g5, n_g5, pa, "yellow")
+    ctx.restore()
+
+    #6
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(x_g6, y_g6)
+    # rotate to engage
+    ctx.rotate(th6)
+    # put it back
+    ctx.translate(-x_g6, -y_g6)
+    spur.Spur(ctx).Gear(x_g6, y_g6, rp_g6, n_g6, pa, "green")
+    ctx.restore()
+
+    #7
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(x_g7, y_g7)
+    # rotate to engage
+    ctx.rotate(th7)
+    # put it back
+    ctx.translate(-x_g7, -y_g7)
+    spur.Spur(ctx).Gear(x_g7, y_g7, rp_g7, n_g7, pa, "orange")
+    ctx.restore()
+    </script>
+    <canvas id="plotarea" width="2800" height="1200"></canvas>
+    </body>
+    </html>
+    '''
+
+        return outString
+
     #@-others
     def default(self):
         sys.exit()
